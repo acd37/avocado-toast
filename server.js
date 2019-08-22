1728c16c398cc040c786cbc2a3ffd5242b859c0a
Alec Down <alecdown@gmail.com>
2019-08-22 15:47:25 -0500

    adds route for  static build

--- server.js
+++ server.js
@@ -30,5 +30,19 @@ require('./routes/api/auth')(app);
 const db = require("./models");
 
 db.sequelize.sync().then(() => {
+    // server static assets if in production
+    if (process.env.NODE_ENV === 'production') {
+        // set static folder
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+    }
+
     app.listen(port, () => console.log(`Server running on port ${chalk.green.bold(port)}!`));
 });
+
+
+
+
